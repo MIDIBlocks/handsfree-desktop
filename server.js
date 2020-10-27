2337d22686769aff6778b76cbc076f10abe44749
Midiblocks <midiblocks@gmail.com>
2020-10-26 22:10:28 -0700

    Moves the mouse and clicks

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 8080 })
+const wss = new WebSocket.Server({ port: 8081 })
 const robot = require('robotjs')
 
 wss.on('connection', (ws) => {
+  console.log('connected...')
+  
+  /**
+   * On message received
+   */
   ws.on('message', (message) => {
     message = JSON.parse(message)
+    if (!message.handsfree) return
+    
+    // Move cursor
+    if (message.action === 'updateCursor') {
+      robot.moveMouse(message.cursor.x, message.cursor.y)
 
-    if (message.status === 'moveMouse') {
-      robot.moveMouse(message.x, message.y)
+      if (message.cursor.state.mouseDown) {
+        robot.mouseClick()
+      }
     }
+  })
 
-    if (message.status === 'mouseClick') {
-      robot.mouseClick()
-    }
+  /**
+   * On disconnect
+   */
+  ws.on('close', () => {
+    console.log('...disconnected')
   })
 })
